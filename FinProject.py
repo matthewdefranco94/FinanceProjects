150f169cf5fb412cc7173fa9725f86c43a662836
Matthew DeFranco <matthewdefranco94@gmail.com>
2021-05-13 23:05:47 -0700

    Refactored, daily retruns now created dataframe

--- FinProject.py
+++ FinProject.py
@@ -23,19 +23,19 @@ class FinCalc():
 
         self.df = pd.DataFrame()
 
-        for stock in self.assets:
-            self.df[stock] = web.DataReader(stock , data_source = 'yahoo' , start = self.stockStartDate , end = self.today)['Adj Close']
-
-
-        self.title = 'Daily Percentage Change'
-
-        self.my_stocks = self.df
 
         # self.stdev = self.df.std()
         # print(self.stdev)
 
     #create and plot graph (loops through each column)
     def daily_returns(self):
+        for stock in self.assets:
+            self.df[stock] = web.DataReader(stock , data_source = 'yahoo' , start = self.stockStartDate , end = self.today)['Adj Close']
+
+
+        self.title = 'Daily Percentage Change'
+
+        self.my_stocks = self.df    
         for columns in self.my_stocks.columns.values:
             self.my_stocks[columns] = self.my_stocks[columns].pct_change(periods = 1)
             plt.plot(self.my_stocks[columns] , label = self.my_stocks)
