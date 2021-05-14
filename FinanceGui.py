6cc732e565cc2bb2288594a7cdefc3689578ece0
Matthew DeFranco <matthewdefranco94@gmail.com>
2021-05-13 22:25:52 -0700

    Recieve input from user to be sent to FinProject

--- FinanceGui.py
+++ FinanceGui.py
@@ -7,6 +7,8 @@ from pandas_datareader import data as web
 import time
 import tkinter as tk
 from tkinter import *
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg , NavigationToolbar2Tk)
+# from FinProject import *
 
 
 
@@ -27,10 +29,33 @@ class StockSim():
         self.input_label = tk.Entry(self.Interface)
         self.input_label.pack()
 
+
+        self.user_input = self.input_label.get()
+
+        print(self.user_input)
+
+
         self.input_button = tk.Button(self.Interface , text = 'Return Results')
-        self.input_button.bind("<KeyRelease>")
+        self.input_button.bind("<ButtonRelease>" , self.stock_input)
         self.input_button.pack()
+        
 
         self.Interface.mainloop()
+
+
+
+    def stock_input(self , event):
+        return self.input_label.get()
+        
+
+        # self.figure = self.plt.Figure(figsize = (6,5) , dpi = 100)
+        # self.chart_type = FigureCanvasTkAgg(figure , Interface)
+        # self.chart_type.get_tk_widget().pack()
+
+
+
+        
+    # def plots():
+    #     break
 
 Gui = StockSim()
